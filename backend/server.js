74e49becc7d52a808a9899fc869aa21955290f3d
Mansi8588 <ajmrhw12@gmail.com>
2025-02-18 13:46:55 +0530

    create user model

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+
+
+app.use(express.json());   // to parse the incoming request with json payloads (from req.body)
+// app.get("/", (req, res) => {
+//   res.send("hello world!");
+// });
 
 
 // THis look ugly so we use middleware for this and us something like
