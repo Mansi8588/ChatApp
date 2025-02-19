12045b4582078334ad4cd61cf7eb5414bcd7cba0
Mansi8588 <ajmrhw12@gmail.com>
2025-02-19 13:53:36 +0530

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -24,6 +27,7 @@ app.use(cookieParser())  // call so that able to access cookies
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
 
 
 
